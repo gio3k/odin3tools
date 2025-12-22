991eb4d6f41f893675abd307a57536e8a4bea3b1
Gio <me@gio.blue>
2025-12-22 18:07:51 +0800

    dump-partitions-from-android: whoops

--- dump-partitions-from-android/dump.sh
+++ dump-partitions-from-android/dump.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Odin's "Run script as Root" feature is weird, so this just bootstraps the dumper script
 # We just download the actual script ("Run script as Root" runs with the working dir as / so we wouldn't know where the script is)
-curl -fsSL https://raw.githubusercontent.com/gio3k/odin3tools/main/odin3tools/dump-partitions-from-android/src/dump-device.sh -o /storage/emulated/0/Download/you-can-delete-this-dump-device.sh
+curl -fsSL https://raw.githubusercontent.com/gio3k/odin3tools/refs/heads/main/dump-partitions-from-android/src/dumper-tool.sh -o /storage/emulated/0/Download/you-can-delete-this-dumper-tool.sh
 mkdir -p /storage/emulated/0/Dumps
-/bin/sh /storage/emulated/0/Download/you-can-delete-this-dump-device.sh > /storage/emulated/0/Dumps/dump-all-output.txt 2>&1
-rm /storage/emulated/0/Download/you-can-delete-this-dump-device.sh
+/bin/sh /storage/emulated/0/Download/you-can-delete-this-dumper-tool.sh > /storage/emulated/0/Dumps/dump-all-output.txt 2>&1
+rm /storage/emulated/0/Download/you-can-delete-this-dumper-tool.sh
